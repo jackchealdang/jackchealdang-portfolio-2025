6cca4443af8320e422ff64fce2ef143bbee4a2dd
Jackcheal Dang <jackchealdang@gmail.com>
2025-02-27 01:04:26 -0600

    feat: add github workflow (actions) for gh pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://jackchealdang.github.io",
+  base: "jackchealdang-portfolio-2025",
+});
